86f8996d2e5ab718e91b35e5f506e030d50c0e81
jintaiahn <78123561+jintaiahn@users.noreply.github.com>
2023-03-01 01:30:18 -0700

    refactored website using react; first commit

---
+++ react_sandbox/my-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Box, Container, Tabs, Tab, Typography} from '@mui/material';
+import HomeContent from './HomeContent';
+
+function App() {
+  const [tab, setTab] = React.useState(0);
+
+  const tabs = ['Home', 'Database Queries', 'Element Analysis Thresholds & Sensitivity Ratings'].map((t) => <Tab label={t}/>);
+
+  const handleChange = (event, newValue) => {
+    setTab(newValue);
+  };
+
+  return (
+    <Container maxWidth='xl'>
+      <Typography variant='h4'>United States Forest Service</Typography>
+      <Typography variant='h6'>National Lichens & Air Quality Database and Clearinghouse</Typography>
+      <Tabs sx={{background: '#95a984'}} value={tab} onChange={handleChange}>
+        {tabs}
+      </Tabs>
+      <Container maxWidth='lg' sx={{marginY: 5}}>
+        <Typography>{`Tab ${tab} selected`}</Typography>
+        <HomeContent hidden={tab !== 0}/>
+        <Box hidden={tab !== 1}>Something</Box>
+      </Container>
+    </Container> 
+  );
+}
+
+export default App;
