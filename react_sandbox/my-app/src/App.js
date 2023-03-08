d3544aacd6dfdf6b53ee1b03146a86b287006e50
Carter Brown <cbdemordaunt@gmail.com>
2023-03-08 09:29:20 -0700

    Home page update
    changing to card format and using an about page for extra information

--- react_sandbox/my-app/src/App.js
+++ react_sandbox/my-app/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {Box, Container, Tabs, Tab, Typography} from '@mui/material';
 import HomeContent from './HomeContent';
 import ElementContent from './ElementContent';
+import AboutContent from './AboutContent';
 
 function App() {
   const [tab, setTab] = React.useState(0);
 
-  const tabs = ['Home', 'Database Queries', 'Element Analysis Thresholds & Sensitivity Ratings'].map((t) => <Tab label={t}/>);
+  const tabs = ['Home', 'Database Queries', 'Element Analysis Thresholds & Sensitivity Ratings', 'About Us'].map((t) => <Tab label={t}/>);
 
   const handleChange = (event, newValue) => {
     setTab(newValue);
@@ -23,6 +24,7 @@ function App() {
         <Typography>{`Tab ${tab} selected`}</Typography>
         <HomeContent hidden={tab !== 0}/>
         <ElementContent hidden={tab !== 2}/>
+        <AboutContent hidden={tab !== 3}/>
         <Box hidden={tab !== 1}>Something</Box>
       </Container>
     </Container> 
