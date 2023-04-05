4cd4fdd78d41f83bffc859e83eae32fc52adca57
jintaiahn <jintaiahn@gmail.com>
2023-04-05 00:38:03 -0600

    resolve merge
    merge resolve for ImageSlider, App.js, and database files

--- react_sandbox/my-app/src/App.js
+++ react_sandbox/my-app/src/App.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import {Box, Container, Tabs, Tab, Typography, Menu, MenuItem, Popover, List, ListItem, ListItemText, ListItemButton, Item, Stack} from '@mui/material';
+import React, { useState } from 'react';
+import {Box, Container, Tabs, Tab, Typography, Menu, MenuItem, MenuList, Stack, AppBar, Popper, Paper} from '@mui/material';
 import HomeContent from './HomeContent';
 import ElementContent from './ElementContent';
 import AboutContent from './AboutContent';
 import './App.css';
 import usfsLogo from './images/usfs-logo.png'
 import TemplateContent from './template';
+import GalleryContent from './GalleryContent';
 
 function App() {
   const [tab, setTab] = React.useState(0);
   const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const [currPage, setCurrPage] = useState("home");
+  const [tab, setTab] = useState(1);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleChange = (event, newValue) => {
     setTab(newValue);
+    setAnchorEl(event.currentTarget);
+    console.log(newValue);
   };
 
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+  const handleClick = (event, pageID) => {
+    setCurrPage(pageID);
+    handleClose()
+    console.log(currPage)
   };
+
   const handleClose = () => {
     setAnchorEl(null);
+    console.log(open);
   };
   function ToolsMenu({open, anchorEl, handleClose, tab}) {
+
+  function ToolsMenu({open, anchorEl, handleClose, handleClick, tab}) {
     return (
       <Menu
+      <Popper
         anchorEl={anchorEl}
         open={open && tab === 1}
         onClose={handleClose}
@@ -33,19 +50,43 @@ function App() {
         <MenuItem onClick={handleClose}>Database Queries</MenuItem>
         <MenuItem onClick={handleClose}>Element Analysis Thresholds & Sensitivity Ratings</MenuItem>
       </Menu>
+        open={open && tab === 2}
+        placement={'bottom-start'}
+        // autoFocus={false}
+        >
+        <Paper>
+          <MenuList onMouseLeave={handleClose}>
+            <MenuItem onClick={handleClose}>Database Queries</MenuItem>
+            <MenuItem onClick={(event) => handleClick(event, "elementAnalysis")}>Element Analysis Thresholds & Sensitivity Ratings</MenuItem>
+          </MenuList>
+        </Paper>
+      </Popper>
     );
   }
+
   function OtherMenu({open, anchorEl, handleClose, tab}) {
     return (
       <Menu
+      <Popper
         anchorEl={anchorEl}
         open={open && tab === 4}
         onClose={handleClose}
+        open={open && tab === 5}
+        placement={'bottom-start'}
+        // autoFocus={false}
       >
         <MenuItem onClick={handleClose}>Fun with Lichens and Air Quality: Projects & Ideas</MenuItem>
         <MenuItem onClick={handleClose}>Lichens and Air Quality Workgroup</MenuItem>
         <MenuItem onClick={handleClose}>Links to Other Resources</MenuItem>
       </Menu>
+        <Paper>
+          <MenuList onMouseLeave={handleClose}>
+            <MenuItem onClick={handleClose}>Fun with Lichens and Air Quality</MenuItem>
+            <MenuItem onClick={handleClose}>Lichens and Air Quality Workgroup</MenuItem>
+            <MenuItem onClick={handleClose}>Links to Other Resources</MenuItem>
+          </MenuList>
+        </Paper>
+      </Popper>
     );
   }
 
@@ -64,11 +105,30 @@ function App() {
         >
           
             <img class='logo' src={usfsLogo} alt='logo'/>
+      <Box onMouseLeave={handleClose} sx={{ bgcolor: '#95a984' }}>
+        <AppBar position={'relative'}>
+          <Tabs sx={{background: '#5b8441'}} value={tab} onChange={handleChange}>
             <Stack
             direction='column'
+              justifyContent='flex-start'
+              direction="row"
+              spacing={2}
+              marginTop={2}
+              marginLeft={2}
+              marginRight={8}
+              marginBottom={2}
             >
               <Typography variant='h4' fontFamily='monospace'>United States Forest Service</Typography>
               <Typography variant='h6' >National Lichens & Air Quality Database and Clearinghouse</Typography>
+              
+                <img class='logo' src={usfsLogo} alt='logo'/>
+                <Stack
+                direction='column'
+                >
+                  <Typography variant='h4' fontFamily='monospace'>United States Forest Service</Typography>
+                  <Typography variant='h6' >National Lichens & Air Quality Database and Clearinghouse</Typography>
+                </Stack>
+              
             </Stack>
           
         </Stack>
@@ -82,15 +142,35 @@ function App() {
           <OtherMenu open={open} anchorEl={anchorEl} handleClose={handleClose} tab={tab}/>
         </Tabs>
         
+            <Tab label="Home" onMouseEnter={(event) => handleChange(event, 1)} onClick={(event) => handleClick(event, "home")} sx={{fontSize: 18, fontWeight: 'bold'}}/>
+            <Tab label="Tools" onMouseEnter={(event) => handleChange(event, 2)} sx={{fontSize: 18, fontWeight: 'bold'}}/>
+            <Tab label="Gallery" onMouseEnter={(event) => handleChange(event, 3)} onClick={(event) => handleClick(event, "gallery")} sx={{fontSize: 18, fontWeight: 'bold'}}/>
+            <Tab label="Reports" onMouseEnter={(event) => handleChange(event, 4)} sx={{fontSize: 18, fontWeight: 'bold'}}/>
+            <Tab label="Other" onMouseEnter={(event) => handleChange(event, 5)} sx={{fontSize: 18, fontWeight: 'bold'}}/>
+            <Tab label="Contact" onMouseEnter={(event) => handleChange(event, 6)} onClick={(event) => handleClick(event, "contact")} sx={{fontSize: 18, fontWeight: 'bold'}}/>
+            {/* <Tab label="Test" value={"test"} onClick={handleClick} sx={{fontSize: 18, fontWeight: 'bold'}}/> */}
+
+            <ToolsMenu open={open} anchorEl={anchorEl} handleClose={handleClose} handleClick={handleClick} tab={tab}/>
+            <OtherMenu open={open} anchorEl={anchorEl} handleClose={handleClose} tab={tab}/>
+          </Tabs>
+        </AppBar>
         <Container maxWidth='lg' sx={{marginY: 5}}>
           <HomeContent hidden={tab !== 0}/>
           <ElementContent hidden={tab !== 2}/>
           <AboutContent hidden={tab !== 3}/>
           <TemplateContent hidden={tab !== 4}/>
+          <HomeContent hidden={currPage !== "home"}/>
+          <GalleryContent hidden={currPage !== "gallery"}/>
+          <ElementContent hidden={currPage !== "elementAnalysis"}/>
+          <AboutContent hidden={currPage !== "contact"}/>
+          <TemplateContent hidden={currPage !== 4}/>
+          {/* <Box hidden={tab !== 5}>Contact content goes here.</Box>
+          <Box hidden={tab !== 6}>database queries page</Box> */}
         </Container>
         <footer style={{opacity: 0}}>.</footer> {/* this is to get rid of the white footer but nothing should actually showup here */}
         </Box>
      
+      </Box>
   );
 }
 
---
+++ react_sandbox/my-app/src/GalleryContent.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import {Box, Typography, ImageList, ImageListItem, Modal, Backdrop} from '@mui/material'
+
+const images = [];
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '60%',
+    height: '80%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 0,
+  };
+
+function importAll(r) {
+  r.keys().forEach((key) => images.push(key));
+}
+
+importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
+export default function GalleryContent({hidden}) {
+    const [open, setOpen] = useState(false);
+    const [selectedImg, setSelectedImg] = useState(null);
+
+    const handleOpen = (src) => {
+        setSelectedImg(src);
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setSelectedImg(null);
+        setOpen(false);
+    }
+
+    return (
+
+        <Box hidden={hidden}>
+            <ImageList variant="masonry" cols={3} gap={10}>
+                {images.map((testImg) => (
+                    <ImageListItem key={testImg} onClick={() => handleOpen(testImg)}>
+                        <img
+                            src={`./images/${testImg}`}
+                            alt={`./images/${testImg}`}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+
+            {selectedImg && (<Modal
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                    backdrop: {
+                        onClick: handleClose,
+                    },
+                }}
+            >
+                <Box sx={style}>
+                    <img
+                        src={`./images/${selectedImg}`}
+                        width={'100%'}
+                        height={'100%'}
+                    />
+                </Box>
+            </Modal>
+            )}
+        </Box>
+    );
+}
