32ab2bb3a773746586308cd270e1060d2fdebcdc
eford8 <36213867+eford8@users.noreply.github.com>
2023-04-10 10:15:32 -0600

    Update app.R

--- Figure1-shiny-app/app.R
+++ Figure1-shiny-app/app.R
@@ -123,9 +123,7 @@ ui <- fluidPage(
   
   ## a download button that pdownloads the map from the page 
   downloadButton("mymapDownload", label = "Download Map"),
-  
-  ## a download button that generates the report that made the map
-  downloadButton("report", "Generate Report")
+ 
 )
 
 
@@ -195,29 +193,7 @@ server <- function(input, output, session) {
   circleMarkerOutline <- function(x)  {
     ifelse(is.na(x), F,"black")
   }
-  
-#   #@Emi's addin for downloading the report --- not tested to work 
-   output$report <- downloadHandler(
-       # For PDF output, change this to "report.pdf"
-       filename = "report.html",
-       content = function(file) {
-         # Copy the report file to a temporary directory before processing it, in
-         # case we don't have write permissions to the current working dir (which
-         # can happen when deployed).
-         tempReport <- file.path(tempdir(), "report.Rmd")
-         file.copy("report.Rmd", tempReport, overwrite = TRUE)
-         # Set up parameters to pass to Rmd document
-         params <- list(n = input$slider)
-
-         # Knit the document, passing in the `params` list, and eval it in a
-         # child of the global environment (this isolates the code in the document
-         # from the code in this app).
-         rmarkdown::render(tempReport, output_file = file,
-           params = params,
-           envir = new.env(parent = globalenv())
-         )
-       }
-     )
+ 
   
   #whenever the user selects a different year range the points and circles are updated
   observe({
